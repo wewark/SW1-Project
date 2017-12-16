ad2c60efc43eb526ed626150b8db3fd922588e27
Sherif <SherifAbdlNaby@gmail.com>
2017-12-16 18:39:20 +0200

    Explore Stores' products with Views sorted

--- src/Controllers/StoreController.java
+++ src/Controllers/StoreController.java
@@ -12,6 +12,9 @@
 package Controllers;
 import Models.*;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class StoreController {
@@ -57,6 +60,24 @@ public class StoreController {
 			while ((i = sc.nextInt()) < 1 || i > store.products.size())
 				System.out.print("Invalid Input");
 
+			return store.products.get(i - 1);
+		}
+		else return null;
+	}
+
+	//Print Views for Store Owners
+	public StoreProduct chooseStoreProductsViews() {
+		Scanner sc = new Scanner(System.in);
+		int i = 0;
+		List<StoreProduct> sortedProducts = new ArrayList<>(store.products);
+		sortedProducts.sort(Comparator.comparingDouble(StoreProduct::getView).reversed());	//TODO Test Sorting
+		if(sortedProducts.size() > 0) {
+			for (StoreProduct product : store.products) {
+				System.out.println(product.view + " Views | " + product.detailsString());
+			}
+			System.out.print("Choose Product: ");
+			while ((i = sc.nextInt()) < 1 || i > store.products.size())
+				System.out.print("Invalid Input");
 			return store.products.get(i - 1);
 		}
 		else return null;
