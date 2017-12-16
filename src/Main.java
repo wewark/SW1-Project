327daa985ca48a13b7b6dfd85a387dc6b49a1f17
Sherif <SherifAbdlNaby@gmail.com>
2017-12-16 08:35:18 +0200

    Now Login Using Sessions

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,27 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-	private static UserController user = new UserController();
 	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		loginOrSignup();
+		loginOrSignup();
+		loginOrSignup();
 		loginOrSignup();
 
 	}
 
 	private static void loginOrSignup() {
 		System.out.println("1. Login\n" +
-				"2. Signup");
+						   "2. Signup");
 		int x = Integer.parseInt(sc.nextLine());
 		if (x == 1) {
 			while (true) {
-				System.out.print("name: ");
+				System.out.print("Username: ");
 				String username = sc.nextLine();
-				System.out.print("password: ");
+				System.out.print("Password: ");
 				String password = sc.nextLine();
-				if (user.login(username, password)) break;
+				if (UserController.login(username, password)) break;
 				System.out.println("Wrong username or password");
 			}
 			System.out.println("Logged in");
@@ -32,14 +34,13 @@ public class Main {
 		else {
 			while (true) {
 				HashMap<String, String> userData = new HashMap<>();
-				System.out.print("name: ");
+				System.out.print("Name: ");
 				userData.put("name", sc.nextLine());
-				System.out.print("username: ");
+				System.out.print("Username: ");
 				userData.put("username", sc.nextLine());
-				System.out.print("password: ");
+				System.out.print("Password: ");
 				userData.put("passwordHash", sc.nextLine());
-
-				if (user.register(userData)) break;
+				if (UserController.register(userData)) break;
 				System.out.println("user exists");
 			}
 			System.out.println("User added");
--- src/Models/User.java
+++ src/Models/User.java
@@ -9,26 +9,33 @@ public class User {
 	public String username;
 	public String birthDate;
 	public String passwordHash;
-	public Address address;
-	public CCInfo CCInfo;
+	public String address;
+	public String CCInfo;
 	public ShoppingCart shoppingCart;
-	public User(HashMap<String, String> userData) {
-
-	}
-
-	public User() {
-
-	}
 
 	/**
 	 * @param user user to be added to DB
 	 */
 	public static void addUser(User user) {
 		user.ID = nextID++;		//TODO get latest ID from DB if we're saving data.
-		Platform.users.add(user);
+		Platform.Users.add(user);
 	}
 
-	public User(String name, String username, String birthDate, String passwordHash, Address address, CCInfo CCInfo) {
+	public User() {
+
+	}
+
+	public User(HashMap<String, String> userData) {
+		this.name = userData.get("name");
+		this.username = userData.get("username");
+		this.birthDate = userData.get("birthDate");
+		this.passwordHash = userData.get("passwordHash");
+		this.address = userData.get("address");
+		this.CCInfo = userData.get("CCInfo");
+		this.shoppingCart = new ShoppingCart();
+	}
+
+	public User(String name, String username, String birthDate, String passwordHash, String address, String CCInfo) {
 		this.name = name;
 		this.username = username;
 		this.birthDate = birthDate;
@@ -43,14 +50,14 @@ public class User {
 	 * @return true if exists
 	 */
 	public static boolean exists(String username) {
-		for (User user : Platform.users)
+		for (User user : Platform.Users)
 			if (user.username.equals(username))
 				return true;
 		return false;
 	}
 
 	public static User getUser(String username) {
-		for (User user : Platform.users)
+		for (User user : Platform.Users)
 			if (user.username.equals(username))
 				return user;
 		return new User();
