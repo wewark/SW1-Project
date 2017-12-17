441d3fb3cef878d4fe2a77e039bbaa4ebd695f79
Khaled Hamed <khaled.hamedt@gmail.com>
2017-12-17 18:29:24 +0200

    Edit login & logout tests

--- test/Controllers/UserControllerTest.java
+++ test/Controllers/UserControllerTest.java
@@ -1,6 +1,9 @@
 package Controllers;
 
+import Models.Platform;
 import Models.Session;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -8,42 +11,32 @@ import java.util.HashMap;
 import static org.testng.Assert.assertEquals;
 
 public class UserControllerTest {
+	private HashMap<String, String> userData = new HashMap<>();
 
-	@Test
-	public void testRegisterUser() throws Exception {
-		HashMap<String, String> userData = new HashMap<>();
-		userData.put("type", "Admin");
+	@BeforeMethod
+	public void setUp() throws Exception {
+		Platform.Users.clear();
+		userData.put("type", "User");
 		userData.put("name", "khaled");
 		userData.put("username", "wewarkUser");
 		userData.put("passwordHash", "khaled");
-		assertEquals(UserController.register(userData), true);
-		assertEquals(UserController.login(userData.get("username"), userData.get("passwordHash")), true);
-		assertEquals(Session.IsLoggedIn(), true);
-		UserController.logout();
-		assertEquals(Session.IsLoggedIn(), false);
 	}
 
 	@Test
-	public void testRegisterStoreOwner() throws Exception {
-		HashMap<String, String> userData = new HashMap<>();
-		userData.put("type", "Admin");
-		userData.put("name", "khaled");
-		userData.put("username", "wewarkStoreOwner");
-		userData.put("passwordHash", "khaled");
+	public void testRegister() throws Exception {
 		assertEquals(UserController.register(userData), true);
 		assertEquals(UserController.login(userData.get("username"), userData.get("passwordHash")), true);
-		assertEquals(Session.IsLoggedIn(), true);
-		UserController.logout();
-		assertEquals(Session.IsLoggedIn(), false);
 	}
 
 	@Test
-	public void testRegisterAdmin() throws Exception {
-		HashMap<String, String> userData = new HashMap<>();
-		userData.put("type", "Admin");
-		userData.put("name", "khaled");
-		userData.put("username", "wewarkAdmin");
-		userData.put("passwordHash", "khaled");
+	public void testLogin() throws Exception {
+		assertEquals(UserController.register(userData), true);
+		assertEquals(UserController.login(userData.get("username"), userData.get("passwordHash")), true);
+		assertEquals(Session.IsLoggedIn(), true);
+	}
+
+	@Test
+	public void testLogout() throws Exception {
 		assertEquals(UserController.register(userData), true);
 		assertEquals(UserController.login(userData.get("username"), userData.get("passwordHash")), true);
 		assertEquals(Session.IsLoggedIn(), true);
